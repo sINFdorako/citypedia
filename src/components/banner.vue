cc7759a121005febe3df8197755ff178c4c0a872
domifab <domi.fab98@gmail.com>
2017-12-04 20:32:13 +0100

    added top banner

---
+++ src/components/banner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-jumbotron id="toptag">
+      <template slot="header">
+        Citypedia
+      </template>
+      <template slot="lead">
+        behind the skyline
+      </template>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#toptag {
+  text-align: left;
+  margin-bottom: 0px;
+
+}
+
+
+</style>
