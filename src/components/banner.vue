755db07d66342eb5dea0d4662d1fd354f8f6f8dd
Dominik Fabisiewicz <domi.fab98@gmail.com>
2017-12-05 14:02:17 +0100

    bugfix

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -17,11 +17,19 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+.unselectable {
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
 #toptag {
+  padding-top: 20px;
   text-align: left;
   margin-bottom: 0px;
-
+  background-image: url("../assets/background.png");
+  height: 280px;
 }
-
-
 </style>
