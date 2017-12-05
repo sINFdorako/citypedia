66fd7caaf5e4d8fee2af246af420a151c9f7b92d
Dominik Fabisiewicz <domi.fab98@gmail.com>
2017-12-05 13:53:41 +0100

    -

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -11,9 +11,7 @@
 <script>
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 
 .unselectable {
     -webkit-touch-callout: none;
@@ -23,23 +21,11 @@
     -ms-user-select: none;
     user-select: none;
 }
-
-
 #toptag {
   padding-top: 20px;
   text-align: left;
   margin-bottom: 0px;
-  /* The image used */
   background-image: url("../assets/background.png");
-/* Set a specific height */
   height: 280px;
-
-  /* Create the parallax scrolling effect */
-/*  background-attachment: fixed;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-*/}
-
-
+}
 </style>
--- src/components/pagenavbar.vue
+++ src/components/pagenavbar.vue
@@ -5,7 +5,7 @@
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
 
-    <b-navbar-brand v-on:click="scrollTop" id="navbrand" href="#"> </b-navbar-brand>
+    <b-navbar-brand v-on:click="scrollTop" href="#"> </b-navbar-brand>
 
 
     <b-collapse is-nav id="nav_collapse">
