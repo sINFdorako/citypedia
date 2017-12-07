6aadb7f13a31825a038f4f0fb5bbee5c8b801171
Dominik Rakowski <dominik.shp@googlemail.com>
2017-12-07 11:47:34 +0100

    update cytiform

--- src/components/addcity/addcity.vue
+++ src/components/addcity/addcity.vue
@@ -3,41 +3,49 @@
     <div class="positioning">
       <h1>Which city do you want to add?</h1>
       <br>
-      <input id="inputField" class="input" type="text" v-model="city" placeholder="city">
+      <input id="inputField" class="input" type="text" v-model="input" placeholder="city">
       <br>
       <div>
-        <b-button :to="{ path: 'citydetails/'+city }" class="button" v-on:click="checkinputcity">go on</b-button>
+        <b-button class="button" v-on:click="checkinputcity">go on</b-button>
       </div>
     </div>
   </div>
-
 </template>
 
 
 <script>
 export default {
   name: 'addcity',
+
   data: function(){
     return {
-      city:'',
+      input: this.input,
     };
   },
 
   methods: {
-      checkinputcity: function (event) {
-        if(event){
-          var inputField = document.getElementById("inputField").value;
-          if (this.city == null) {
-            alert("Please tell us the name of your city before you go on")
-          }
 
-        }
+      checkinputcity: function (event){
+        if(event){
+            var inputField = document.getElementById("inputField").value;
+            if (this.$data.input == null) {
+              alert("Please tell us the name of your city before you go on")
+        }else{
+          this.$router.push({ path: 'citydetails/' +this.$data.input})
       }
     }
+  }
+}
 }
 
 
 //:to="{ path: 'citydetails/'+city }"
+
+//}else{
+  //router.push({ path: 'citydetails/' +city })
+//}
+
+
 </script>
 
 <style scoped>
