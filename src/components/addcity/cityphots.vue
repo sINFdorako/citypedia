ca10163085fb9c9400656c7630aa3860ec936f7c
Dominik Rakowski <dominik.shp@googlemail.com>
2017-12-08 16:25:19 +0100

    update city

---
+++ src/components/addcity/cityphots.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="positioning">
+      <b-button class="button" v-on:click="back" > back </b-button>
+      <b-button  class="button" v-on:click="finish" >finish</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cityphots',
+  data: function(){
+    return {
+       input: this.$route.params.input,
+       population: this.$route.params.population,
+       area: this.$route.params.area,
+       country: this.$route.params.country
+    };
+  },
+  methods: {
+
+      back: function(event) {
+        if(event){
+          this.$router.go(-1)
+        }
+      },
+
+      finish: function(event){
+        if(event){
+          alert("are you sure you are finished?")
+        }
+      }
+    }
+}
+
+</script>
+
+<style scoped>
+
+.positioning{
+  margin-top: 25pt;
+  text-align: center
+}
+
+</style>
