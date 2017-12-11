1238e26dd8057f835bd059530d3c9e8d28ac0d87
Dominik Rakowski <dominik.shp@googlemail.com>
2017-12-11 09:50:20 +0100

    update

--- src/components/addcity/cityphots.vue
+++ src/components/addcity/cityphots.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
+
+    <h1 style="text-align: center; margin-top: 30pt;">Add some pictures of {{input}}</h1>
+
+    <div class="positioning">
+      <img src="../../assets/fileuploader-dragdrop-icon.png"/>
+    </div>
+
+    <!--<div class="center">
+    <vue-core-image-upload
+    class="btn btn-primary"
+    :crop="false"
+    @imageuploaded="imageuploaded"
+    :data="data"
+    :max-file-size="5242880"
+    url="http://101.198.151.190/api/upload.php" >
+    </vue-core-image-upload>
+</div>-->
+
     <div class="positioning">
       <b-button class="button" v-on:click="back" > back </b-button>
       <b-button  class="button" v-on:click="finish" >finish</b-button>
     </div>
+
   </div>
+
 </template>
+
 
 <script>
 export default {
@@ -15,7 +36,9 @@ export default {
        input: this.$route.params.input,
        population: this.$route.params.population,
        area: this.$route.params.area,
-       country: this.$route.params.country
+       country: this.$route.params.country,
+       image: '',
+        src: 'http://img1.vued.vanthink.cn/vued0a233185b6027244f9d43e653227439a.png'
     };
   },
   methods: {
@@ -31,15 +54,38 @@ export default {
           alert("are you sure you are finished?")
         }
       }
-    }
-}
+    },
+
+    imageuploaded(res) {
+      if (res.errcode == 0) {
+        this.src = res.data.src;
+      }
+    },
+
+  /*components: {
+     'vue-core-image-upload': VueCoreImageUpload,
+   },*/
+  }
 
 </script>
 
 <style scoped>
 
+body {
+				font-family: 'Roboto', sans-serif;
+				font-size: 14px;
+                line-height: normal;
+				color: #47525d;
+				background-color: #fff;
+
+				margin: 0;
+				padding: 20px;
+
+				width: 560px;
+			}
+
 .positioning{
-  margin-top: 25pt;
+  margin-top: 100pt;
   text-align: center
 }
 
--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ Vue.use(BootstrapVue);
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { FileUploadService } from 'v-file-upload'
 
 Vue.config.productionTip = false
 
