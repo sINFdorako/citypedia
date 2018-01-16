68300db70e0da0a97efea10481d730357e817589
Dominik Rakowski <dominik.shp@googlemail.com>
2018-01-16 13:46:25 +0100

    view

--- src/components/view.vue
+++ src/components/view.vue
@@ -1,16 +1,39 @@
+
 <template>
-<div>
-  <h1 style="text-align: center; margin-top: 50pt;">Stadtname</h1>
 
-  <hr />
+  <div>
+    <h1 style="text-align: center; margin-top: 50pt;">{{city.cityname}}</h1>
 
-  <img src="" alt="image" />
+    <hr />
 
-  <h3>Population</h3>
-</div>
+    <img :src="city.link" alt="image" />
+
+    <h3>Population: {{city.population}}</h3>
+  </div>
+
 </template>
 
 <script>
+
+export default {
+  name: 'view',
+  created: function(){
+    this.$http.get('http://localhost:3000/api/city')
+    .then(function(resp) {
+      this.city = resp.body;
+    })
+    .catch(function(err) {
+      this.cities = "Something went wrong: " +err
+    })
+
+},
+  data:
+  function(){
+    return {
+       cities: []
+    };
+  },
+}
 
 </script>
 
