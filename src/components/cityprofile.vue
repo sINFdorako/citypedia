53cdd22f96f8a0c7be71d903eb8cb4c5c6710f7b
Dominik Fabisiewicz <domi.fab98@gmail.com>
2018-01-16 13:13:42 +0100

    citylist added, and its functional GG

--- src/components/cityprofile.vue
+++ src/components/cityprofile.vue
@@ -2,10 +2,10 @@
 <div>
   <div class="positioning">
 
-    <div class="card" style="width: 20rem;">
+    <div class="card" v-for="city in cities" style="width: 20rem;">
       <img class="card-img-top" src="https://media-cdn.tripadvisor.com/media/photo-s/0e/9a/e3/1d/freedom-tower.jpg" alt="Card image cap">
       <div class="card-block">
-        <h4 id="heading1" class="card-title">New York</h4>
+        <h4 id="heading1" class="card-title">{{city.cityname}}</h4>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
@@ -21,19 +21,12 @@ export default {
   created: function(){
     this.$http.get('http://localhost:3000/api/cities')
     .then(function(resp) {
-
       this.cities = resp.body;
-      console.log(resp.body);
     })
     .catch(function(err) {
-
-      this.countries = "Something went wrong: " +err
+      this.cities = "Something went wrong: " +err
     })
 
-    $(document).ready(function() {
-
-    $(".card-title").text('hallos');
-  })
 },
   data:
   function(){
